f5214291ecfe85989434c1d71ed8c76e36bf9db5
abhisheksah1 <rajusah7253@gmail.com>
2024-07-04 08:34:51 +0545

    update

--- frontend/src/pages/MemberSection.jsx
+++ frontend/src/pages/MemberSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import img1 from "../assets/abhishek.jpg";
-import img2 from "../assets/priyanka.png";
-import img3 from "../assets/hritik.jpg";
-import img4 from "../assets/rahul.jpg";
+import img2 from "../assets/sushil.jpg";
+import img3 from "../assets/rohan.jpg";
+import img4 from "../assets/shivam.jpg";
 
 import MemberSection1 from "./MemberSection1";
 
@@ -13,9 +13,7 @@ export default function MemberSection() {
         <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">
           People who made it successful
         </h2>
-        <p className="mx-auto mt-4 max-w-2xl text-gray-600 md:text-xl">
-          
-        </p>
+        <p className="mx-auto mt-4 max-w-2xl text-gray-600 md:text-xl"></p>
       </div>
       <div className="mt-8  grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
         <MemberSection1
@@ -30,28 +28,30 @@ export default function MemberSection() {
         />
         <MemberSection1
           imgLink={img2}
-          memberName={"Priyanka Kumari "}
+          memberName={"Sushil Thakur"}
           memberPosition={"Helper"}
           facebookLink={
-            "https://www.facebook.com/profile.php?id=100055496693741"
+            "https://www.facebook.com/profile.php?id=100080029884090"
           }
-          instaLink={"https://www.instagram.com/priyankasah446/"}
+          instaLink={"https://www.instagram.com/sushill.aa/"}
           aboutMember={"Helping our Team to improve"}
         />
         <MemberSection1
           imgLink={img3}
-          memberName={"Hritik Jaiswal"}
+          memberName={"Rohan Thakur"}
           memberPosition={"Project Manager"}
-          facebookLink={"https://www.facebook.com/hritikjaiswal.com.np"}
-          instaLink={"https://www.instagram.com/hritikjaiswal.com.np/"}
+          facebookLink={"https://www.facebook.com/raghbendra.thakur.71"}
+          instaLink={"https://www.instagram.com/iamnotrohan_/"}
           aboutMember={"Managing the project"}
         />
         <MemberSection1
           imgLink={img4}
-          memberName={"Rahul Jaiswal"}
+          memberName={"Shivam Kumar Sah"}
           memberPosition={"Designer & Contributor"}
-          facebookLink={"https://www.facebook.com/rahul.jaishwal.9235"}
-          instaLink={"https://www.instagram.com/rahul_jaiswal4123/"}
+          facebookLink={
+            "https://www.facebook.com/profile.php?id=100062786209367"
+          }
+          instaLink={"https://www.instagram.com/shivam_shah05/"}
           aboutMember={" Design of MeroCV "}
         />
       </div>
